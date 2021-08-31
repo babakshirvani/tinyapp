2e0eeea94f2e76a20d91ba20371ccfcc5c65054d
Babak Shirvani <bk.shirvani@gmail.com>
2021-08-31 01:04:25 +0000

    Add a POST Route to Receive the Form Submission, and add function generateRandomString

--- express_server.js
+++ express_server.js
@@ -4,6 +4,12 @@ const PORT = 8080;
 
 app.set("view engine", "ejs");
 
+function generateRandomString() {
+  const result = Math.random().toString(36).substring(2, 8);
+  console.log(result)
+  return result;
+};
+
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselab.ca",
   "9sm5xK": "http://www.google.com"
@@ -36,6 +42,11 @@ app.get("/urls.json", (req, res) => {
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.listen(PORT, () => {
