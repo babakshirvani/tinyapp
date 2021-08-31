11e501c847a79a105429aa7b1eb7c67cdf3f82ed
Babak Shirvani <bk.shirvani@gmail.com>
2021-08-31 02:41:16 +0000

    Add Redirect Short URLs

--- express_server.js
+++ express_server.js
@@ -45,8 +45,15 @@ app.get("/hello", (req, res) => {
 });
 
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
-  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+  const shortURL = generateRandomString();
+  let requestBody = req.body;
+  urlDatabase[shortURL] = requestBody.longURL;
+  res.redirect(`/u/${shortURL}`);
+});
+
+app.get("/u/:shortURL", (req, res) => {
+  const longURL = urlDatabase[req.params.shortURL];
+  res.redirect(longURL);
 });
 
 app.listen(PORT, () => {
